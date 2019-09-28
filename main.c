31c74440e8dc9b04c214ffab5bb401741c74de67
vmod <42954251+msidqi@users.noreply.github.com>
2019-09-28 13:18:14 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void	*print_thread_range(void *param)
 {
 	t_data *data;
 
-	data = param;
+	data = (t_data *)param;
 	printf("thread_range start: x %f y %f\n", data->thread_range.start.x, data->thread_range.start.y);
 	printf("thread_range   end: x %f y %f\n", data->thread_range.end.x, data->thread_range.end.y);
 	return (NULL);
@@ -23,6 +23,6 @@ int main(void)
 	// 100 by 100 window
 	data.winwidth = 100;
 	data.winheight = 100;
-	ft_multi_thread(&data, print_thread_range);
+	ft_multi_thread(&data, &print_thread_range);
 	return (0);
 }
